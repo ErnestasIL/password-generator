f60cfc428a6763e2377ab91ff7d1c641b6af1844
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-07 19:02:27 +0200

    created DOM elements and constants

---
+++ script.js
@@ -0,0 +1,12 @@
+const lowercase = "abcdefghijklmnoprstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "`~!@#$%^&*()_+=][}{|'.,><?;:";
+
+const lengthInput = document.querySelector(".length");
+const uppercaseChecked = document.querySelector(".upper");
+const numbersChecked = document.querySelector(".numbers");
+const symblosChecked = document.querySelector(".symbols");
+const generateButton = document.querySelector(".generate");
+const passwordBox = document.querySelector(".password");
+const copyButton = document.querySelector(".copy");
